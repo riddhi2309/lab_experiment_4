6a0926a9fcb7275f02dbe413a0c54abdffc869e9
riddhi2309 <141670096+riddhi2309@users.noreply.github.com>
2023-09-11 10:55:05 +0530

    question_09.py

--- question_09.py
+++ question_09.py
@@ -1,32 +1,33 @@
-#getting the input by the user ''sentence
-sentence =(input("Enter a stentence:-"))
-
-#ititializing of the variale with zero 
-i = 0
-upercase = 0
-lowercase = 0
-digit = 0
-special = 0
-
-#starting the while loop where i is less than len(sentence)
-while  i < len(sentence):
-    char = sentence[i]
-
-#starting the if condition      
-    if char.isupper():
-        upercase += 1
-    elif char.islower():
-        lowercase += 1
-    elif char.isdigit():
-        digit +=1
+#getting the input by the user 'char'
+char = input("Enter the string : ")
+#creating the list 'up','low','digit','specase'
+up = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+low = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+digit = ["0",'1','2','3','4','5','6','7','8','9']
+specase=["!","@","#","$","%",'^','&',"*","/","?","~"]
+#intializing the variable
+up_i = 0
+low_i = 0
+digit_i = 0
+specase_i = 0
+#creating the new variable to count the length of the char 
+length=len(char)
+i=0
+#starting the while loop by stating the 
+while i<length:
+    if char[i] in up:
+        up_i=up_i+1
+    elif char[i] in low:
+        low_i=low_i+1
+    elif char[i] in digit:
+        digit_i=digit_i+1
     else:
-        special += 1
-    i += 1
-
-#printing the answers    
-print("Uppercase letters:", upercase)
-print("Lowercase letters:", lowercase)
-print("Digits:", digit)
-print("Special characters:", special)        
+        specase_i=specase_i+1
+    i=i+1       
+#printing the result
+print("UPPERCASE",up_i)
+print("lowercase",low_i)
+print("Digits",digit_i)
+print("Special Characters",specase_i)        
 
         
